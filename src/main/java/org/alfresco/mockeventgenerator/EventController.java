4acca9d895853d9ad06126de2904c27ea1cc9289
Gheorghiu Flavian <gheorghiu.flavian@yahoo.com>
2018-12-21 14:08:55 +0200

    BUILDER-90: Retouched import statements

--- src/main/java/org/alfresco/mockeventgenerator/EventController.java
+++ src/main/java/org/alfresco/mockeventgenerator/EventController.java
@@ -24,7 +24,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * @author Jamal Kaabi-Mofrad
